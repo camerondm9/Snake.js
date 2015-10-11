31b3b3607a0af259ac0132bb1dd5673f0c996a1d
Cameron Martens <camerondm9@yahoo.ca>
2015-10-10 23:42:48 -0500

    Corrected live url

--- snake-client.js
+++ snake-client.js
@@ -1,4 +1,4 @@
-var config = {websocketUrl: "ws://snake-camerondm9.rhcloud.com:8080/"};
+var config = {websocketUrl: "ws://snake-camerondm9.rhcloud.com/"};
 var snake = {lastFrame: 0,
 			guideOpacity: 0,
 			guideShow: false,
